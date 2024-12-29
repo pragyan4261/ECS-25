bfa39de5e28fc422c2815ae6cd138b00c6e9575c
Nikk6174 <nikk.6174@gmail.com>
2024-12-30 00:20:11 +0530

    msg

--- Backend-ECS/src/utils/APIError.js
+++ Backend-ECS/src/utils/APIError.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class APIError extends Error{
     constructor(
         statusCode,
@@ -20,26 +20,4 @@ class APIError extends Error{
     }
 }
 export {APIError}
-=======
-class APIError extends Error{
-    constructor(
-        statusCode,
-        message="Something Went Wrong",
-        error=[],
-        stack="",
-    ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success= false
-        this.error=error;
-        if(stack){
-            this.stack=stack
-        }else{
-            Error.captureStackTrace(this,this.constructor)
-        }
-    }
-}
-export {APIError}
->>>>>>> d7aa76d64fe0b6d3299a44becd680ebf0512e45f
+
