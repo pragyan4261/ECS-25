d8fbb49d0b5fd5e5cab174fc6892b8546994683e
Nikk6174 <nikk.6174@gmail.com>
2025-01-02 14:31:56 +0530

    authentication button done

--- Frontend-ECS/src/pages/Header.jsx
+++ Frontend-ECS/src/pages/Header.jsx
@@ -15,10 +15,17 @@ export default function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="/sign-in"
                             className="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Login
+                        </Link>
+
+                        <Link
+                            to="/sign-up"
+                            className="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Sign Up
                         </Link>
                     </div>
                     <div
