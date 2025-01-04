34256d76a0e53f792217af2ec677cc2359bf9a50
Subrata-20 <subrata_ug_23@ece.nits.ac.in>
2025-01-05 00:15:33 +0530

    Adding Gallery and Navbar

--- Frontend-ECS/src/components/Header.jsx
+++ Frontend-ECS/src/components/Header.jsx
@@ -1,114 +1,130 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const [hoveredLink, setHoveredLink] = useState(null);
+
+    const handleHover = (link) => {
+        setHoveredLink(link);
+    };
+
     return (
         <header className="shadow sticky z-50 top-0">
-            <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <nav className="bg-transparent to-black px-4 lg:px-6 py-3">
+                <div className="flex justify-between items-center mx-auto max-w-screen-xl">
+                    {/* Logo Section */}
                     <Link to="/" className="flex items-center">
                         <img
                             src="https://i.postimg.cc/RZrxm2s0/ecs-new-logo-1-black-bg-2.png"
-                            className="mr-3 h-12"
-                            alt="Logo"
+                            className="h-[68px]"
+                            alt="ECS Logo"
                         />
                     </Link>
-                    <div className="flex items-center lg:order-2">
-                        <Link
-                            to="/sign-in"
-                            className="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Login
-                        </Link>
 
-                        <Link
-                            to="/sign-up"
-                            className="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                    {/* Navigation Links */}
+                    <ul className="flex space-x-6 text-white text-sm font-semibold gap-[67px]">
+                        <li
+                            onMouseEnter={() => handleHover('annual-attraction')}
+                            onMouseLeave={() => handleHover(null)}
                         >
-                            Sign Up
-                        </Link>
-                    </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                    to="/"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-blue-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/about"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-orange-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/events"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-orange-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    Events
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/members"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-orange-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    Members
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/developers"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-orange-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    Developers
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/Resource+Hub"
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${
-                                            isActive ? "text-orange-500" : "text-gray-500"
-                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-400 lg:p-0`
-                                    }
-                                >
-                                    Resource Hub
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
+                            <Link
+                                to="/annual-attraction"
+                            >
+                                <img src={hoveredLink === 'annual-attraction'? "https://i.postimg.cc/XqND1Jng/Group-174-5.png" : "https://i.postimg.cc/BQNn6Hxp/Group-174-2.png"} alt="" />
+                            </Link>
+                        </li>
+                        <li
+                            onMouseEnter={() => handleHover('resource-hub')}
+                            onMouseLeave={() => handleHover(null)}
+                            className='transition-transform duration-1000'
+                        >
+                            <Link
+                                to="/resource-hub"
+                            >
+                                <img src={hoveredLink === 'resource-hub'? "https://i.postimg.cc/Gpk7cY2Q/Group_174_(4).png" : "https://i.postimg.cc/mgD2Fkjs/Group-174-6.png"} alt="" />
+                            </Link>
+                        </li>
+                        <li
+                            onMouseEnter={() => handleHover('members')}
+                            onMouseLeave={() => handleHover(null)}
+                        >
+                            <Link
+                                to="/members"
+                            >
+                                <img src={hoveredLink === 'members'? "https://i.postimg.cc/Gp0gwzct/Group-174-1.png" : "https://i.postimg.cc/VsWYGnHw/Group-174-7.png"} alt="" />
+                            </Link>
+                        </li>
+                        <li
+                            onMouseEnter={() => handleHover('developers')}
+                            onMouseLeave={() => handleHover(null)}
+                        >
+                            <Link
+                                to="/developers"
+                            >
+                                <img src={
+                                        hoveredLink === 'developers'
+                                            ? "https://i.postimg.cc/2SL9KgNg/Group-174.png"
+                                            : "https://i.postimg.cc/jSZwGBgs/Group-174-3.png"
+                                    } alt="" className='group-hover:hidden'/>
+                            </Link>
+                        </li>
+                    </ul>
+
+                    {/* User Icon */}
+                    <Link to="/profile" className="flex items-center">
+                        <img src="https://i.postimg.cc/VLJVrcth/user-svgrepo-com-1.png" alt="" />
+                    </Link>
                 </div>
             </nav>
+            {/* Lines */}
+            <div
+                id="1"
+                className={`absolute top-7 left-[353px] h-[2px] w-[22%] bg-gradient-to-r from-[#048AB2] from-70% to-transparent ${
+                    hoveredLink === 'annual-attraction' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="2"
+                className={`absolute top-7 left-[580px] h-[2px] w-[25%] bg-gradient-to-r from-transparent from-5% via-[#048AB2] to-transparent to-95% ${
+                    hoveredLink === 'resource-hub' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="3"
+                className={`absolute top-7 left-[805px] h-[2px] w-[24%] bg-gradient-to-r from-transparent from-5% via-[#048AB2] to-transparent to-95% ${
+                    hoveredLink === 'members' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="4"
+                className={`absolute top-7 rotate-180 left-[1061px] h-[2px] w-[14.7%] bg-gradient-to-r from-[#048AB2] from-70% to-transparent ${
+                    hoveredLink === 'developers' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="5"
+                className={`absolute top-[60px] left-[353px] h-[2px] w-[20%] bg-gradient-to-r from-[#048AB2] from-70% to-transparent ${
+                    hoveredLink === 'annual-attraction' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="6"
+                className={`absolute top-[60px] left-[580px] h-[2px] w-[25%] bg-gradient-to-r from-transparent from-5% via-[#048AB2] to-transparent to-95% ${
+                    hoveredLink === 'resource-hub' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="7"
+                className={`absolute top-[60px] left-[805px] h-[2px] w-[24%] bg-gradient-to-r from-transparent from-5% via-[#048AB2] to-transparent to-95% ${
+                    hoveredLink === 'members' ? 'hidden' : ''
+                }`}
+            ></div>
+            <div
+                id="8"
+                className={`absolute top-[60px] rotate-180 left-[1061px] h-[2px] w-[14.7%] bg-gradient-to-r from-[#048AB2] from-70% to-transparent ${
+                    hoveredLink === 'developers' ? 'hidden' : ''
+                }`}
+            ></div>
         </header>
     );
 }
