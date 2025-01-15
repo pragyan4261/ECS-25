f576c757d9a384e13e24cca7ba7473444d8c5bb5
Maverick-04507 <Shashwatp673@gmail.com>
2025-01-15 09:47:25 +0530

    spline removed

--- Frontend-ECS/src/pages/home/Home.jsx
+++ Frontend-ECS/src/pages/home/Home.jsx
@@ -3,29 +3,23 @@ import Gallery from './Gallery';
 import Message from './Message';
 import AboutUs from './AboutUs';
 import Annual from './Annual';
-import Spline from '@splinetool/react-spline';
 
 export default function Home() {
     return (
         <div className="mx-auto w-full space-y-10 overflow-x-hidden">
-
-            <div className='3d flex justify-center items-center relative '>
-                <Spline scene="https://prod.spline.design/DZKI2wXU4Oob3ycH/scene.splinecode" />
-               
-                    <h2 className="absolute flex justify-center items-center text-white text-4xl font-bold sm:text-5xl">
+            <aside className="main h-[89vh] bg-[url('https://i.postimg.cc/QCWYndrJ/bgImage.png')] bg-cover bg-center bg-no-repeat flex justify-center items-center text-center">
+                <div>
+                    <h2 className="text-white text-4xl font-bold sm:text-5xl">
                         Electronics and Communication Society
                     </h2>
-                
-            </div>
-
-            <div className="cover bg-black flex h-[55px] w-[150px] absolute right-[20px] top-[675px] mobile:right-[10px] mobile:top-[165px]">
-            </div>
-
-          
+                </div>
+            </aside>
             <Message/>
             <AboutUs/>
             <Annual/>
             <Gallery/>
         </div>
+        
     );
+    
 }
