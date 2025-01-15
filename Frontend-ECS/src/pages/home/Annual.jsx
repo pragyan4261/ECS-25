e5e49166360ae80e86ac6fdd7b404bc88cc73bd5
Maverick-04507 <Shashwatp673@gmail.com>
2025-01-16 01:29:39 +0530

    Annual & Event Update

--- Frontend-ECS/src/pages/home/Annual.jsx
+++ Frontend-ECS/src/pages/home/Annual.jsx
@@ -9,118 +9,287 @@ const Annual = () => {
   
   const data = [
     {
-      title: "Spectrum",
+      title: "ECE Orientation",
       description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years old.",
+        "This is a LIT event focusing on literature and creative arts.",
       image:
-        "https://i.postimg.cc/L5WrC3tH/19-20250115-115513-0002.png",
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
     },
     {
-      title: "Radiance",
+      title: "Utkrishtha",
       description:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+        " Join us for an evening of artistic expression and creativity.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
     },
     {
-      title: "Aurora",
-      description:
-        "The passage is attributed to a section 1.10.32 of 'de Finibus Bonorum et Malorum' by Cicero, written in 45 BC.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    {
-      title: "Eclipse",
+      title: "Spectre-Annual Magazine",
       description:
         "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
     },
     {
-      title: "Eclipse",
+      title: "Dehleez",
       description:
         "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
     },
     {
-      title: "Spectrum",
+      title: "Utkrishtha",
       description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years old.",
+        " Join us for an evening of artistic expression and creativity.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
     },
     {
-      title: "Radiance",
-      description:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    {
-      title: "Aurora",
-      description:
-        "The passage is attributed to a section 1.10.32 of 'de Finibus Bonorum et Malorum' by Cicero, written in 45 BC.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    ,
-    {
-      title: "Eclipse",
-      description:
-        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    {
-      title: "Spectrum",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years old.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    {
-      title: "Radiance",
-      description:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },
-    {
-      title: "Aurora",
-      description:
-        "The passage is attributed to a section 1.10.32 of 'de Finibus Bonorum et Malorum' by Cicero, written in 45 BC.",
-      image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
-    },,
-    {
-      title: "Eclipse",
+      title: "Spectre-Annual Magazine",
       description:
         "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
     },
     {
-      title: "Spectrum",
+      title: "Dehleez",
       description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years old.",
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
     },
     {
-      title: "Radiance",
+      title: "Utkrishtha",
       description:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+        " Join us for an evening of artistic expression and creativity.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
     },
     {
-      title: "Aurora",
+      title: "Spectre-Annual Magazine",
       description:
-        "The passage is attributed to a section 1.10.32 of 'de Finibus Bonorum et Malorum' by Cicero, written in 45 BC.",
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       image:
-        "https://res.cloudinary.com/dqlnb4ddv/image/upload/v1736681210/bangkok-city-sunrise-thailand_lfnmni.jpg",
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
     },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
+    },
+    {
+      title: "Utkrishtha",
+      description:
+        " Join us for an evening of artistic expression and creativity.",
+      image:
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
+    },
+    {
+      title: "Spectre-Annual Magazine",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
+    },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
+    },
+    {
+      title: "Utkrishtha",
+      description:
+        " Join us for an evening of artistic expression and creativity.",
+      image:
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
+    },
+    {
+      title: "Spectre-Annual Magazine",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
+    },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
+    },
+    {
+      title: "Utkrishtha",
+      description:
+        " Join us for an evening of artistic expression and creativity.",
+      image:
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
+    },
+    {
+      title: "Spectre-Annual Magazine",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
+    },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
+    },
+    {
+      title: "Utkrishtha",
+      description:
+        " Join us for an evening of artistic expression and creativity.",
+      image:
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
+    },
+    {
+      title: "Spectre-Annual Magazine",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
+    },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },{
+      title: "ECE Orientation",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/xdYFR1kp/Whats-App-Image-2025-01-15-at-19-29-40-435b3f50.jpg",
+    },
+    
+    {
+      title: "EC:ArQ",
+      description:
+        "This is a LIT event focusing on literature and creative arts.",
+      image:
+        "https://i.postimg.cc/rpBGJhYV/Whats-App-Image-2025-01-15-at-19-48-18-45437702.jpg",
+    },
+    {
+      title: "Utkrishtha",
+      description:
+        " Join us for an evening of artistic expression and creativity.",
+      image:
+        "https://i.postimg.cc/CLCJ3Yys/MJ-9663.jpg",
+    },
+    {
+      title: "Spectre-Annual Magazine",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://i.postimg.cc/N0zCw7JS/Whats-App-Image-2025-01-15-at-19-39-43-7a9a294b.jpg",
+    },
+    {
+      title: "Dehleez",
+      description:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      image:
+        "https://instagram.fmaa8-1.fna.fbcdn.net/v/t39.30808-6/440323056_17908104839953783_8745844986834733664_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fmaa8-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RY82HdMN09IQ7kNvgGxmJsW&_nc_gid=67de5aba14ed48adb3137bf410660165&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzM1ODEzNjAzNDc1MDUyNTc0Ng%3D%3D.3-ccb7-5&oh=00_AYBCIzKWCHFqwkrhB1SjLnhdi2XHCt5F3JPeiyJpMfh-FA&oe=678D96F4&_nc_sid=fc8dfb",
+    },
+    
   ];
 
   return (
@@ -132,7 +301,7 @@ const Annual = () => {
         />
       </div>
 
-      <div className="w-full h-[80vh] mx-auto relative">
+      <div className="w-full h-[80vh] mx-auto relative ">
         <Swiper
           autoplay={{ delay: 2000, disableOnInteraction: false }}
           effect={"coverflow"}
@@ -164,17 +333,16 @@ const Annual = () => {
           {data.map((item, index) => (
             <SwiperSlide key={index}>
               <div
-                className="relative w-[90%]  mx-auto p-4 bg-gradient-to-b from-[#0f1347] to-[#103360] shadow-lg rounded-[20px]  border-blue-500"
+                className="relative w-[90%]  p-4 bg-gradient-to-b from-[#0f1347] to-[#103360] shadow-lg rounded-[20px]  border-blue-500"
               >
                 <div
-                 className="mx-auto mt-6 h-48 w-full bg-cover bg-center rounded-[20px]"
-                 style={{
-                 backgroundImage: `url(${item.image})`,
-                 border: "2px solid #007bff",
-                 boxShadow: "0 0 10px 2px #007bff, inset 0 0 5px #007bff",
+                  className="mx-0 mt-6 h-48 w-full px-4 bg-cover bg-center rounded-[20px]"
+                  style={{
+                    backgroundImage: `url(${item.image})`,
+                    border: "2px solid #007bff",
+                    boxShadow: "0 0 10px 2px #007bff, inset 0 0 5px #007bff",
                   }}
-                  ></div>
-
+                ></div>
                 <div className="p-6">
                   <h2 className="text-xl font-bold text-blue-400 text-center">
                     {item.title}
